4e93a9db5cb9672ce4ec5698cec3dcf7b51c1b4c
ebkera <ebk_era@hotmail.com>
2019-12-05 13:29:30 -0600

    Deleted some lines that were commented and not useful

--- coordinateMaker.py
+++ coordinateMaker.py
@@ -100,23 +100,6 @@ class CoordinateMaker():
         # self.conventional_cell_alloy_1.append([0.000, 1.000, 0.000, "A"])  # Index:0 atom at 000 (fcc_a)
         # self.conventional_cell_alloy_1.append([1.000, 1.000, 1.000, "A"])  # Index:0 atom at 000 (fcc_a)
 
-        # self.conventional_cell_alloy_2 = []
-        # # This is the conventional (FCC) unit cell for a diamond lattice
-        # self.conventional_cell_alloy_2.append([0.000, 0.000, 0.000, "B"])  # Index:0 atom at 000 (fcc_a)
-        # self.conventional_cell_alloy_2.append([0.250, 0.250, 0.250, "C"])  # Index:1 basis atom connected to (fcc_b)
-        # # self.conventional_cell_alloy_2.append([0.000,0.500,0.500]) # Index:2 three atoms forming the lattice vectors (fcc_a). # This atom can be removed since slotting another box will duplicate this atom
-        # # self.conventional_cell_alloy_2.append([0.500,0.000,0.500]) # Index:3 # This atom can be removed since slotting another box will duplicate this atom
-        # # self.conventional_cell_alloy_2.append([0.500,0.500,0.000]) # Index:4 # This atom can be removed since slotting another box will duplicate this atom
-        # # self.conventional_cell_alloy_2.append([1.000,1.000,0.000]) # Index:4,5 last atom on the xy plane (fcc_a)
-        # self.conventional_cell_alloy_2.append([0.750, 0.750, 0.250, "C"])  # Index:5,6 last atom on the xy_.25 plane (fcc_b)
-        # self.conventional_cell_alloy_2.append([0.500, 1.000, 0.500, "A"])  # Index:6,7 last two atoms on the 0.5 xy plane (fcc_a)
-        # self.conventional_cell_alloy_2.append([1.000, 0.500, 0.500, "B"])  # Index:8
-        # self.conventional_cell_alloy_2.append([0.250, 0.750, 0.750, "C"])  # Index:7,9 The last two atoms on the 0.75 xy plane (fcc_b)
-        # self.conventional_cell_alloy_2.append([0.750, 0.250, 0.750, "C"])  # Index:8,10
-        # # self.conventional_cell_alloy_2.append([0.000,1.000,1.000]) # Index:9,11 three atoms forming z=1 xy plane (fcc_a) # This atom can be removed since slotting another box will duplicate this atom
-        # # self.conventional_cell_alloy_2.append([1.000,0.000,1.000]) # Index:10,12 # This atom can be removed since slotting another box will duplicate this atom
-        # self.conventional_cell_alloy_2.append([0.500, 0.500, 1.000, "A"])  # Index:11,13
-
         self.radius = radius
         self.replicate = replicate
         self.lattice_constant = lattice_constant
