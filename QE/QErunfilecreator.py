ab974a032368df4f84657de716356666fc73a353
github <erathnayake@SIVALAB-019.SivaLab.sivalab.com>
2019-12-24 12:18:20 -0600

    added the cell_paramter block

--- QE/QErunfilecreator.py
+++ QE/QErunfilecreator.py
@@ -25,6 +25,7 @@ class QERunCreator:
         self.atomic_species = [["Sn", "118.71", "Sn.UPF"]]
         self.lat_const = [6.5]
         self.celldm = [0, 12.394714, 0, 0, 0, 0, 0] # celldm variables. index 0 is not used and index 1,6 corresponds to celldm1,6
+        self.cell_parameters = False
         self.set_of_files = []
         self.ibrav = 2
         self.nat = 2
@@ -97,10 +98,10 @@ class QERunCreator:
             file.write(f"/\n")
             file.write(f"&system\n")
             file.write(f"    ibrav           = {self.ibrav}\n")
-            if a != False:
-                # Works only if a lattice parameter has been set.
+            if a != False and self.cell_parameters == False:
+                # Works only if a lattice parameter has been set. and the cell_parameter_block is not set
                 file.write(f"    celldm(1)       = {a}\n")
-            else:
+            elif a != False and self.cell_parameters == False:
                 for x in range(1,6):
                     if self.celldm[x] != 0:
                         file.write(f"    celldm({x})       = {self.celldm[x]}\n")
@@ -121,10 +122,22 @@ class QERunCreator:
             file.write(f"/\n")
             file.write(f"&electrons\n")
             file.write(f"    mixing_beta     = 0.7\n")
-            file.write(f"/\n")
+
+            # This is for the cell_parameter_block
+            if self.cell_parameters != False:
+                file.write(f"\n")
+                file.write(f"CELL_PARAMETERS\n")
+                for vector in self.cell_parameters:
+                    file.write(f"{vector[0]}   {vector[1]}   {vector[2]}\n")
+
+            # This is for the ATOMIC_SPECIES block
+            file.write(f"\n")
             file.write(f"ATOMIC_SPECIES\n")
             for specie in self.atomic_species:
                 file.write(f"{specie[0]}   {specie[1]}   {specie[2]}\n")
+
+            # This is for the ATOMIC_POSITIONS block
+            file.write(f"\n")
             file.write(f"ATOMIC_POSITIONS alat \n")
             # Here we have to make srue that the new requrired supercell (basis) is loaded into the file
             if self.supercell_file == "default":
@@ -154,6 +167,14 @@ class QERunCreator:
                 file.write(f"#  kx, ky, kz, wk  (wk=symmetry weight is ignored in bands calculations)\n")
                 file.write(self.k_path)
         shutil.move(f"{file_name}.in", f"./{dirname}/{file_name}.in")
+
+
+    def normalize(self):
+        pass
+
+
+
+
 
     def create_run(self, walltime, together):
         self.k_file_reader()
