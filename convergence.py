bee7549170a9c388e4dd3b74ae1db03ffd82ee82
github <erathnayake@SIVALAB-019.SivaLab.sivalab.com>
2020-01-09 15:23:43 -0600

    added the changing from Ry to J

--- convergence.py
+++ convergence.py
@@ -4,21 +4,24 @@ import sys
 import subprocess
 import time
 import matplotlib
-# matplotlib.use('Agg')  # no UI backend required if working in the wsl without a UI
+matplotlib.use('Agg')  # no UI backend required if working in the wsl without a UI
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import numpy as np
+from ebk import Rydberg2J
+from ebk import Rydberg2eV
 
 class LatticeConstantOptimize():
     def __init__(self, a, E, name="a_optimization"):
         """
         Takes the lattice constants[list in Angstroms], energies[list in eV] and a name[for .out files] as inputs
         """
+        print(f"LatticeConstantOptimize: E = {E}")
         self.a = a
-        self.e = E
-        self.v = [n**3/4.0 for n in self.a]   # There are four primitive cells in a single conventional cell
+        self.e = [Rydberg2J(x) for x in E]  # everything is in Jules now
+        print(f"LatticeConstantOptimize: E = {self.e}")
+        self.v = [n**3 for n in self.a]   # There are four primitive cells in a single conventional cell this is for a diamond/zinc blende structure
         self.name = name
-                
         #Any other parameters you can set here
         self.graph_title = "Optimization of lattice constant"
 
@@ -42,16 +45,15 @@ class LatticeConstantOptimize():
         self.min_index = np.argmin(self.y_fit)
         self.a0_optimized = self.x_fit.flat[self.min_index]
         self.v0_optimized = self.v_x_fit.flat[self.min_index]      # There are four primitive cells in a single conventional cell
-        self.v0_optimized = 6.672**3/4.0      # There are four primitive cells in a single conventional cell
-
+        # self.v0_optimized = 6.672**3/4.0      # There are four primitive cells in a single conventional cell
         # self.v0_optimized = self.a0_optimized**3/4.0      # There are four primitive cells in a single conventional cell
 
         # Calculations
-        # Getting the double derivative using a 3rd degree polynomial
+        # Getting the double derivative using a 2nd degree polynomial
         self.dda0 = 2*self.z[0]#.flat[0]
         self.ddv0 = 2*self.vz[0]#.flat[0]
         # self.ddv0 = 6*self.vz.flat[0]*self.v0_optimized + 2*self.vz.flat[1]
-        self.B = self.v0_optimized*self.ddv0#*160.21766208  # 1 eV/Angstrom3 = 160.21766208 GPa
+        self.B = self.v0_optimized*self.ddv0/10E6#*160.21766208  # 1 eV/Angstrom3 = 160.21766208 GPa
 
     def plot(self):
         fit_label = f"a$_0$: {round(self.a0_optimized,3)} $\\AA$, B$_0$: {round(self.B,3)} GPa, $\\Omega_0$:{round(self.v0_optimized,3)} $\\AA^3$"
