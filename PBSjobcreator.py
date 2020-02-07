6571bbd42a1bbd52066f8d81d762dc9e4d6f6c40
ebkera <ebk_era@hotmail.com>
2020-02-06 20:35:58 -0600

    Initializations

--- PBSjobcreator.py
+++ PBSjobcreator.py
@@ -1,1 +1,7 @@
 """This is module creates jobs for running on a PBS job scheduler"""
+
+class PBSjobcreator:
+    """This is the class for creating PBS jobs"""
+    def __init__(*args, **kwargs):
+        """Init method"""
+        pass
