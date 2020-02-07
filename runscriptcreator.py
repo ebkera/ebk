c4d6d91dda7c80aee333a00545b1fe23c1c373b0
ebkera <ebk_era@hotmail.com>
2020-02-06 20:40:34 -0600

    Initial commit

---
+++ runscriptcreator.py
@@ -0,0 +1,7 @@
+"""This file creates bash scripts for running on CARBON. This can be used to run multiple jobs for example"""
+
+class Runscriptcreator:
+    """Doc string goes here"""
+    def __init__(*args, **kwargs):
+        """Doc string goes here"""
+        pass
