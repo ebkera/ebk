0ec7477c1c3d760d3c1abccfc2a8c9c1d2a6e83c
ebkera <erathnayake@SIVALAB-019.SivaLab.sivalab.com>
2020-02-14 14:12:16 -0600

    Major edits

--- calculation_set.py
+++ calculation_set.py
@@ -9,19 +9,42 @@ class Calculation_set():
 
     def __init__(self, *args,**kwargs):
         """
-        Defined here are all the parameters that you will have to set.
-
+        Defined here are all the parameters that you will have to initialize.
         kwargs:
         calculator: string
             Sets the calculator to be used to create runs. Multiple types cannot be set (will have to create two Calculation_set objects).
-            Recognized inputs are : esprsso
-
-
+            Recognized inputs are : espresso, siesta
         """
 
         # Here all the args are set
-        
 
         # Here all the kwargs are set
-        self.calculator = kwargs.get("calculator", "espresso")  
+        self.run_name = kwargs.get("name", "run")
+        self.calculator = kwargs.get("calculator", "espresso")
+        self.a0 = kwargs.get("a0", [6.6])
+        self.KE_cut = kwargs.get("KE_cut", [20])
+        self.k = kwargs.get("k", [2])
+        self.pseudopotentials = kwargs.get("pseudopotentials", {'Sn': 'Sn_ONCV_PBE_FR-1.1.upf'})
+
+        # Here goes the PBS init stuff
+        self.walltime_mins = 30
+        self.nodes = 2
+        self.procs = 8
+
+        # Initializations
+        self.d = f"^"  # Here you can set the desired delimiter
+        self.equals = f"="  # Here you can set the desired symbol for value assigner
+
+        # Some other calc that needs to be done
+        
+
+        def make_runs(*args, **kwargs):
+            """This method makes all the runs that are required for a single calculation"""
+        for KE_cut_i in self.KE_cut:
+            for a0_i in self.a0:
+                for k_i in self.k:
+                    for R_i in self.R:
+                        run_name = f"QE{d}KE{KE_cut_i}{d}K{k_i}{d}R{R_i}{d}a{a0_i}{d}PP={self.PP}{d}calc={calc}{d}{}"
+                        self.structure.set_cell([(b, 0, 0), (0, b, 0), (0, 0, b)], scale_atoms=True)
+                        ase.io.write(f"{run_name}.in", bulk, format = "espresso-in", 
 
