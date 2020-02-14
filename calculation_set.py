968198ae627a6e43ebdcb2db167baeec0f6628bf
ebkera <erathnayake@SIVALAB-019.SivaLab.sivalab.com>
2020-02-14 09:57:06 -0600

    Some initializations

--- calculation_set.py
+++ calculation_set.py
@@ -1,3 +1,20 @@
 """
 This file is intended to replace all run creator files
 """
+
+class Calculation_set():
+    """
+    This class creates an object that will create runs. It will depend entirely on what machine you will run and all that.
+    """
+
+    def __init__(self, *args,**kwargs):
+        """
+        Defined here are all the parameters that you will have to set.
+        """
+
+        # Here all the args are set
+        
+
+        # Here all the kwargs are set
+        self.calculator = kwargs.get("calculator", "espresso")
+
