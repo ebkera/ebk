3b51ffdbbebbbc2f984cfb8ec74d94b5c4a0f10a
ebkera <ebk_era@hotmail.com>
2020-04-30 12:16:07 -0500

    Fixed bug

--- __init__.py
+++ __init__.py
@@ -126,8 +126,6 @@ def get_pseudopotential(id):
     #LDA pps
     elif identifier == "QE_PZ_FR_NLCC_1":
             pseudopotential = {'Sn': f"Sn.rel-pz-dn-kjpaw_psl.0.2.UPF"}
-    elif identifier == "MCT_Slab_EDT":
+    elif identifier == "MCT_Slab_EDT" or identifier == "MCT_Slab_BDT12" or identifier == "MCT_Slab_BDT14":
         pseudopotential = {'C': f"c_pbe_v1.2.uspp.F.UPF", "H":"h_pbe_v1.4.uspp.F.UPF", "Hg":"hg_pbe_v1.uspp.F.UPF", "S":"s_pbe_v1.4.uspp.F.UPF", "Te":"te_pbe_v1.uspp.F.UPF"}
     return pseudopotential
-
-    
