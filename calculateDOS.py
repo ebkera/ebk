944e81fa56fa9615516b1be2b58eaba55a595e57
ebkera <ebk_era@hotmail.com>
2020-05-16 18:56:56 -0500

    Minor cosmetic

--- calculateDOS.py
+++ calculateDOS.py
@@ -1,5 +1,5 @@
 """
-This Files needs the eig2dos file to working
+This Files needs the eig2dos file to work
 """
 import os
 import matplotlib
@@ -113,14 +113,12 @@ class Band():
         self.E_f = 0
         self.kpath_k = []
         self.kpath_symbol = []
-  
 
-        
     def automate(self):
         self.modify_eig_file(0.05, 1000, -3, 3)
         self.make_DOS_file()
-        self.load_from_dos()    
-        
+        self.load_from_dos()
+
     def modify_eig_file(self, eta, ne, emin, emax):
         """Opens the EIG file and then saves the modified version of it
         eta  - I'd call it precision, put it less than 1. 
@@ -131,7 +129,7 @@ class Band():
         for line in file:
             rows.append(line)
         file.close()
-        
+
         # This is to catch if the first row is not a float. If it is will proceed. If had been edited we get a warning and will still replace the old parameters with new ones
         try:
             self.E_f = float(rows[0].strip())
@@ -148,8 +146,7 @@ class Band():
     
     def make_DOS_file(self):
         os.system("./eig2dos  < " + self.file_name_eig + " > " + self.file_name_dos)
-        
-        
+
     def load_from_dos(self):
         """Opens the files to read"""
         file = open(self.file_name_dos,"r")
