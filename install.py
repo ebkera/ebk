a48a0170927864b5e8be11350a4c497777f3fc78
ebkera <ebk_era@hotmail.com>
2020-05-17 13:40:12 -0500

    added logging information

--- install.py
+++ install.py
@@ -1,4 +1,8 @@
 """This file will create contains installation scripts"""
+import logging
+
+logging.basicConfig(filename='installation_log.txt', level=logging.DEBUG, format='\
+%(asctime)s - %(levelname)s - %(message)s')
 
 class Install():
     def __init__(self, source_folder):
@@ -29,3 +33,6 @@ class InstallSIESTA(Install):
 
     def install(self):
         pass
+
+if __name__ == "__main__":
+    pass
