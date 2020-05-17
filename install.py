170a686765aa26ff7a4bd062e22b4ab30631adb5
ebkera <ebk_era@hotmail.com>
2020-05-17 12:38:17 -0500

    initial commit

---
+++ install.py
@@ -0,0 +1,31 @@
+"""This file will create contains installation scripts"""
+
+class Install():
+    def __init__(self, self.source_folder):
+        self.source_folder = self.source_folder
+
+    def check(self):
+        pass
+
+    def install(self):
+        pass
+
+class InstallSIESTA(Install):
+    def __init__(self):
+        pass
+
+    def check(self):
+        pass
+
+    def install(self):
+        pass
+
+class InstallSIESTA(Install):
+    def __init__(self):
+        pass
+
+    def check(self):
+        pass
+
+    def install(self):
+        pass
