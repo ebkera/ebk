576afb49634dab514e7329126a1644d5468fdff2
ebkera <ebk_era@hotmail.com>
2020-10-05 08:52:50 -0500

    added createshellnp and create_relaxed

--- MatMan/NPs.py
+++ MatMan/NPs.py
@@ -3,6 +3,7 @@ from ase.io import read, write
 from ase import Atoms
 from ebk.SIESTA import read_struct_file, struct2xyz
 from ebk import get_machine_paths
+import numpy as np
 # from ebk.coordinateMaker import
 
 def analyze_bonds(file_name, central_atom_index = 0, passivant = "H", dot_species = "Sn"):
@@ -92,10 +93,140 @@ def analyze_bonds(file_name, central_atom_index = 0, passivant = "H", dot_specie
     print(f"Diameter: {diameter} (Angstroms)")
     write_to_log()
 
+def create_shell_NP(file_name, central_atom_index = 0, passivant = "H", dot_species = "Sn", bond_length = False, N_shells = 1):
+    """
+    eads in an XYZ file.
+    This function does the following,
+    Creates a given number of shells for a given nano-particle
+    """
+    fname = file_name.split(".")
+    del fname[-1]
+    fname = ".".join(fname)
+    
+    # Reading in the file
+    atoms_ase = read(file_name)
+    coordinate_format = "Angstroms"
+    atoms = []
+    central_atom = atoms_ase[central_atom_index]
+    if not bond_length: bond_length = atoms_ase.get_distance(0, 1)
+    bond_length_threshold = 1.2*atoms_ase.get_distance(0, 1)
+    surface_radii = []
+    no_of_NP_atoms = 0
+    no_of_passivant_atoms = 0
+    surface_atom_indeces = []
+    passivant_atom_indeces = []
+    for i, atom in enumerate(atoms_ase):
+        if atom.symbol == dot_species:
+            no_of_NP_atoms+=1
+        elif atom.symbol == passivant:
+            no_of_passivant_atoms += 1
+            passivant_atom_indeces.append(i)
+            continue
+        # list_to_append = []
+        # surface = False
+        # list_to_append.append(atom)
+        # list_to_append.append(atoms_ase.get_distance(0, i))
+        # Looking for nearest neighbours
+        for j, atom2 in enumerate(atoms_ase):
+            if i != j and atoms_ase.get_distance(i, j) < bond_length_threshold:
+                # list_to_append.append(atom2)
+                if atom2.symbol == "H":
+                    if i not in surface_atom_indeces:   # to avoind appendign teh same surface atoms that is connected to multiple passivation atoms
+                        surface_atom_indeces.append(i)
+                    atom.symbol = "S"
+        # list_to_append.append(surface)
+        # atoms.append(list_to_append)
+
+    # Printing out the NP with surface atoms highlighted
+    write(f"{fname}_SA.xyz", atoms_ase)
+    #removing the H atoms
+    passivant_atom_indeces.sort(reverse = True) # sorting in reverse order in anticipation of deletion
+    for i in passivant_atom_indeces:
+        del(atoms_ase[i])
+    write(f"{fname}_!PA.xyz", atoms_ase)
+
+    # Now starts the layer growing stuff
+
+    default_positions = [[bond_length,bond_length,bond_length],[-bond_length,-bond_length,bond_length],[-bond_length,bond_length,-bond_length],[bond_length,-bond_length,-bond_length]]
+    added_atoms_list = []
+
+    for layer in range(1,N_shells+1):
+        print(f"Working on layer: {layer}")
+
+        for i in surface_atom_indeces:
+            # print(atoms_ase[i].position)
+            radial_dist = atoms_ase.get_distance(0, i)
+            for pos in default_positions:
+                new_position = [atoms_ase[i].position[0] + pos[0], atoms_ase[i].position[1] + pos[1], atoms_ase[i].position[2] + pos[2]]
+                new_dist = np.sqrt(new_position[0]**2+new_position[1]**2+new_position[2]**2)
+                if new_dist < radial_dist: continue
+                atoms_ase.append("C") # for newly added
+                atoms_ase[-1].position = [atoms_ase[i].position[0] + pos[0], atoms_ase[i].position[1] + pos[1], atoms_ase[i].position[2] + pos[2]]
+                added_atoms_list.append(len(atoms_ase)-1)
+        print(added_atoms_list)
+
+        # cleaning up the unwanted atoms
+        atoms_to_delete = []
+        for i in added_atoms_list:
+            for j, atom2 in enumerate(atoms_ase):
+                if i != j and atoms_ase.get_distance(i, j) < bond_length_threshold:
+                    atoms_to_delete.append(j)
+
+    write(f"{fname}_final.xyz", atoms_ase)
+
+
+def create_relaxed(file_name, replace_with):
+    """
+    Reads in an XYZ file.
+    This function does the following,
+    Creates a given number of shells for a given nano-particle
+    replacewith should also be a file name.
+    """
+    fname = file_name.split(".")
+    del fname[-1]
+    fname = ".".join(fname)
+    
+    # Reading in the file
+    atoms_ase = read(file_name)
+    replace_with = read(replace_with)
+
+    bond_length_threshold = atoms_ase.get_distance(0, 1)
+
+    list_to_delete = []
+    for i, atom in enumerate(replace_with):
+        if atom.symbol == "H":
+            list_to_delete.append(i)
+    list_to_delete.sort(reverse = True)
+    for i in list_to_delete:
+        del(replace_with[i])
+
+    list_to_delete = []
+    for i, atom in enumerate(replace_with):
+        for j, atom2 in enumerate(atoms_ase):
+            if atom2.symbol == "H": continue
+            distx = abs(atom.position[0]-atom2.position[0])
+            disty = abs(atom.position[1]-atom2.position[1])
+            distz = abs(atom.position[2]-atom2.position[2])
+            dist = np.sqrt(distx**2+disty**2+distz**2)
+            if dist < 0.5*bond_length_threshold: 
+                print(f"ase: {atom2} is close to replace atom {atom}")
+                list_to_delete.append(j)
+
+    list_to_delete.sort(reverse = True)
+    for i in list_to_delete:
+        del(atoms_ase[i])
+
+    atoms_ase.extend(replace_with)
+
+    write(f"{fname}_SemiRelaxed.xyz", atoms_ase)
+
+
+
+
 class Insert_NP():
     """
     This class is made to be strictly for ligands so that we dont have to go about attaching ligands manually. 
-    This class will isert a ligand at a given position and at a given orientation.
+    This class will insert a ligand at a given position and at a given orientation.
     """
     def __init__(self, *args, **kwargs):
         self.species = kwargs.get("species", "Sn")
