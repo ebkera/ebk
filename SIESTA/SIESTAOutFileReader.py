03600709fc8071a30c98ae2757e0ee8cd9b4b9bd
ebkera <ebk_era@hotmail.com>
2021-04-02 15:23:15 -0500

    added number of atoms

--- SIESTA/SIESTAOutFileReader.py
+++ SIESTA/SIESTAOutFileReader.py
@@ -6,22 +6,117 @@ class SiestaReadOut():
     def __init__(self, system_label):
         self.system_label = system_label
         self.file = []
+        self.Species = [["Label", "Atomic_Number_int", "Number_of_atoms_int"]]  # Logs all labels and atomic numbers. Since we have a example value at index=0 Index will serve as species number
+
+        # Here are defaults
+        trigger_read_in_atoms = False
+
         f = open(f"{self.system_label}.out", "r")
         for line in f:
             self.file.append(line)
         f.close()
 
+        # We will be reading all values here...
+        for line in self.file:
+            #Getting the number of types of atoms and masses from line example "Species number:   1 Atomic number:   50 Label: Sn"
+            if "Species number:" in line and "Atomic number:" in line:
+                temp_Species = []
+                x = line.strip("Species number:")
+                x = x.split(":")
+                x[0] = x[0].strip("Atomic number")
+                x[1] = int(x[1].strip("Label"))
+                x[2] = x[2].strip()
+                self.Species.append([x[2], x[1], 0])
+                # print(x)
+
+            # Here we will read in vacuum levels
+            if "dhscf: Vacuum level (max, mean)" in line:
+                x = line.strip("dhscf: Vacuum level (max, mean)=")
+                x = x.split()
+                self.Vac_max = float(x[0])
+                self.Vac_mean = float(x[1])
+                self.Vac_units = x[2]
+
+            # Reading fermi level here
+            if "siesta:         Fermi =" in line:
+                x = line.strip("siesta:         Fermi =")
+                x = x.split()
+                self.Ef = float(x[0])
+
+            # Here we are reading in the Total energies
+            if "siesta:         Total =" in line:
+                x = line.strip("siesta:         Total =")
+                x = x.split()
+                self.E_total = float(x[0])      
+
+            # Here we are reading in the number of atoms and orbitals and projectors
+            if "initatomlists: Number of atoms, orbitals, and projectors:" in line:
+                x = line.strip("initatomlists: Number of atoms, orbitals, and projectors:")
+                x = x.split()
+                self.N_atoms = int(x[0])
+                self.N_orbitals = int(x[1])
+                self.N_projectors = int(x[2])
+
+            # Here we read in atoms should be below the actual reading in script
+            if trigger_read_in_atoms:
+                x = line.split()
+                # print(x)
+                if len(x) != 6: 
+                    trigger_read_in_atoms = False
+                    continue
+                self.Species[int(x[4])][2] += 1
+
+
+            # Here we set trigger to read in atoms (initial coordinates) should be below the actual reading in script
+            if "siesta: Atomic coordinates (Bohr) and species" in line: trigger_read_in_atoms = True
+            
+        # Here we are getting the Work functions
+        self.WF = self.Vac_mean - self.Ef
+        # print(self.N_atoms, self.N_orbitals, self.N_projectors)
+        # print(self.Species)
+
+    def get_vacuum(self):
+        """returns [self.Vac_max, self.Vac_mean, self.Vac_units]""" 
+        return [self.Vac_max, self.Vac_mean, self.Vac_units]
+
+    def get_total_energy(self):
+        """returns self.E_total"""
+        return self.E_total
+
+    def get_fermi(self):
+        """returns self.Ef"""
+        return self.Ef
+
+    def get_number_of_atoms(self, label = ""):
+        """
+        Returns the number of atoms of 'label' type if 'label' is null returns total number of atoms
+        """
+        atom_type = ""
+        atom_type = label
+        if atom_type == "": return self.N_atoms
+        for x in range(len(self.Species)):
+            if self.Species[x][0] == atom_type: return self.Species[x][2]
+        # result = filter(lambda x: x[0] == atom_type, self.Species)
+        
+    def get_number_of_orbitals(self):
+        """return self.N_orbitals"""
+        return self.N_orbitals
+
+    def get_number_of_projectors(self):
+        """return self.N_projectors"""
+        return self.N_projectors
+
+# From here below we have deprecated code kept for back-compatibility
     def read_vacuum(self):
         """
+        |Deprecated
+        |
         |This function sets the maximum and the mean values of the vacuum and the units. 
         |Inputs:
         |    None
         |Outputs:
         |    None
         """
-        self.Vac_max = 0
-        self.Vac_mean = 0
-        self.Vac_units = 0
         for line in self.file:
             if "dhscf: Vacuum level (max, mean)" in line:
                 x = line.strip("dhscf: Vacuum level (max, mean)=")
@@ -33,6 +128,8 @@ class SiestaReadOut():
 
     def read_fermi(self):
         """
+        |Deprecated
+        |
         |This function reads the fermi level. For now it is set only to read in eV
         |Inputs:
         |    None
@@ -49,6 +146,8 @@ class SiestaReadOut():
 
     def calculate_work_function(self):
         """
+        |Deprecated
+        |        
         |This function calculates the work functions and saves in self.WF 
         |Inputs:
         |    None
@@ -67,13 +166,15 @@ class SiestaReadOut():
 
     def read_total_energy(self):
         """
+        |Deprecated
+        |
         |This function reads the total_energy. For now it is set only to read in eV
         |Inputs:
         |    None
         |Outputs:
         |    None
         """
-        self.Ef = 0
+        # self.Ef = 0
         for line in self.file:
             if "siesta:         Total =" in line:
                 x = line.strip("siesta:         Total =")
