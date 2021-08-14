7bef3dea17beed5ffcdb40b0343f06d4d352bd05
ebkera <ebk_era@hotmail.com>
2021-08-14 06:15:55 -0500

    can now read .EIG file

--- SIESTA/SIESTAOutFileReader.py
+++ SIESTA/SIESTAOutFileReader.py
@@ -1,24 +1,36 @@
 """
-This file reads the the file system_label.out and extracts/calculates data/values from it
+This file reads the the file out_file_name.out and extracts/calculates data/values from it
 """
 
 class SiestaReadOut():
-    def __init__(self, system_label):
-        self.system_label = system_label
-        self.file = []
+    def __init__(self, out_file_name):
+        self.out_file_name = out_file_name
+        self.out_file = []
+        self.EIG_file = []
         self.Species = [["Label", "Atomic_Number_int", "Number_of_atoms_int"]]  # Logs all labels and atomic numbers. Since we have a example value at index=0 Index will serve as species number
+        
+        # Eig file stuff
+        self.Ef_EIG = 0   # Fermi level as read in from eig
+        self.eigen_values_by_k_point = [[]]  # Evey k point will have a set of eigen values. We have already filled one here for the 0th k point so from now index_of_list_element=kpoint
 
         # Here are defaults
         trigger_read_in_atoms = False
 
-        f = open(f"{self.system_label}.out", "r")
+        f = open(f"{self.out_file_name}.out", "r")
         for line in f:
-            self.file.append(line)
+            self.out_file.append(line)
+        f.close()
+        f = open(f"{self.out_file_name}.EIG", "r")
+        for line in f:
+            self.EIG_file.append(line)
         f.close()
 
         # We will be reading all values here...
-        for line in self.file:
+        for line in self.out_file:
             #Getting the number of types of atoms and masses from line example "Species number:   1 Atomic number:   50 Label: Sn"
+            if "SystemLabel" in line:
+                x = line.split()
+                self.SystemLabel = x[1]
             if "Species number:" in line and "Atomic number:" in line:
                 temp_Species = []
                 x = line.strip("Species number:")
@@ -70,6 +82,42 @@ class SiestaReadOut():
             # Here we set trigger to read in atoms (initial coordinates) should be below the actual reading in script
             if "siesta: Atomic coordinates (Bohr) and species" in line: trigger_read_in_atoms = True
             
+
+# Here we are parsing in the .EIG file
+        for i,line in enumerate(self.EIG_file):
+            if i == 0:
+                self.Ef_EIG = float(line)
+                fermi_diff = abs(self.Ef - self.Ef_EIG)
+                if fermi_diff >= 0.001:
+                    print(f"Warning!: Fermi levels in the out file and EIG files don't match |{self.Ef_EIG} - {self.Ef}| = {fermi_diff}")
+            if i == 1:
+                x = line.split()
+                self.NumberOfBands = int(x[0])
+                self.NumberOfSpins = int(x[1])
+                self.NumberOfkPoints = int(x[2])
+            if i > 1:
+                x = line.split()
+                if len(x) == 11:
+                    # This is a fresh K point line
+                    new_eig_vals = []
+                    for j,v in enumerate(x):
+                        if j > 0:
+                            new_eig_vals.append(float(v))                           
+                else:
+                    for j,v in enumerate(x):
+                        new_eig_vals.append(float(v))
+                self.eigen_values_by_k_point.append(new_eig_vals) 
+
+        # While we are at it lets calculate the HOMO and LUMO levels. Not intelligent enough to identify conductors so keep in mind
+        for i,x in enumerate(self.eigen_values_by_k_point[1]):
+            if x >= self.Ef:
+                self.band_index_LUMO = i+1       # Adding 1 to the index so that we the index starts from 1
+                self.band_index_HOMO = (i-1)+1   # Adding 1 to the index so that we the index starts from 1
+                break
+
+                                        
+
+# From here on we are doing methods
         # Here we are getting the Work functions
         try: 
             self.WF = self.Vac_mean - self.Ef
@@ -107,6 +155,27 @@ class SiestaReadOut():
     def get_number_of_projectors(self):
         """return self.N_projectors"""
         return self.N_projectors
+
+    def get_number_of_bands(self):
+        """return self.NumerOfBands"""
+        return self.NumberOfBands
+
+    def get_number_of_spins(self):
+        """return self.NumberOfSpins"""
+        return self.NumberOfSpins
+
+    def get_number_of_k_points(self):
+        """return self.NumberOfkPoints"""
+        return self.NumberOfkPoints
+
+    def get_HOMO_band_index(self):
+        """Calculates the index of the HOMO level"""
+        return self.band_index_HOMO
+
+    def get_LUMO_band_index(self):
+        """Calculates the index of the LUMO level"""
+        return self.band_index_LUMO
+
 
 # From here below we have deprecated code kept for back-compatibility
     def read_vacuum(self):
