4883f3d72ad8b05b5530de0c953e087211ddf680
ebkera <ebk_era@hotmail.com>
2021-11-12 22:35:53 -0600

    added the band curvature calculations

--- VASP/VASPOutFileReader.py
+++ VASP/VASPOutFileReader.py
@@ -1,6 +1,7 @@
 """
 This file reads the the file system_label.out and extracts/calculates data/values from it
 """
+from os import P_WAIT, kill
 from ebk.BandPlotter import BandPlotter 
 
 class VASPReadOut():
@@ -12,6 +13,8 @@ class VASPReadOut():
         self.k_dist=[0]
         self.highest_valance = [[0,0,0], -500]    # Will contain [[kpathpoint],E]
         self.lowest_conduction = [[0,0,0], 500]  # Will contain [[kpathpoint],E]
+        self.valance_band_label_index = 0
+        self.conduction_band_label_index = 0
         self.hsp = []
         self.hss = []
         self.Eg = "notset"
@@ -80,9 +83,11 @@ class VASPReadOut():
                     if Occ > Occ_threshold:  # This is because there might be partial occupancies
                         if E >= self.highest_valance[1]: 
                             self.highest_valance[1] =  E
+                            self.valance_band_label_index = band_label
                     else: # This is because there might be partial occupancies
                         if E<= self.lowest_conduction[1]:
                             self.lowest_conduction[1] = E
+                            self.conduction_band_label_index = band_label
                             # print(E)
 
         for x in range(1,len(self.kpoints)):
@@ -153,7 +158,110 @@ class VASPReadOut():
         if self.draw_band_edge_lines:
             x.extra_horizontal_lines = [[self.lowest_conduction[1], f"Conduction band edge"]]
             x.extra_horizontal_lines.append([self.highest_valance[1], f"Valance band edge"])
-
-
         return x
 
+
+    def get_curvature_at_k_point(self, k_point: list[float,float,float]=False, k_index:int=False, direction:str=False, sigma:int=20, high_symmetry_point:bool=True, show_plot:bool=False)-> float:
+        """
+        This code is still being written.
+        Goals: to get the band curvature around the maxima and minima of the CBM and VBM
+        k_point: 
+        direction: A string of length 2
+        high_symmetry_point: set to true to accomodate for VASP k points being repeated in the k-path at highsymmetry points
+        """
+        print(f"Calculating curvature of the bands")
+        import numpy as np
+        import scipy.constants
+
+        if k_point:
+            pass
+
+        if k_index:
+            print(f"Calculating around k point: {self.kpoints[k_index]}")
+            print(f"Please check that valance and conduction band indeces are: {self.valance_band_label_index+1, self.conduction_band_label_index+1}")
+            right_side_conduction_E = [self.bands[self.conduction_band_label_index][x] for x in range(k_index, k_index+sigma)]
+            right_side_conduction_E_ori = right_side_conduction_E.copy()
+            right_side_kpoint_dist = [self.k_dist[x] for x in range(k_index, k_index+sigma)]
+            right_side_kpoint_dist_delta = right_side_kpoint_dist[-1] - right_side_kpoint_dist[-2]
+            # prepending
+            for x in range(1,len(right_side_conduction_E)):
+                right_side_conduction_E.insert(0,right_side_conduction_E_ori[x])
+                right_side_kpoint_dist.insert(0,right_side_kpoint_dist[0]-right_side_kpoint_dist_delta)
+            if high_symmetry_point:
+                left_side_conduction_E = [self.bands[self.conduction_band_label_index][x] for x in range(k_index-sigma, k_index)]
+                left_side_conduction_E_ori = left_side_conduction_E.copy()
+                left_side_kpoint_dist = [self.k_dist[x] for x in range(k_index-sigma, k_index)]
+            else:
+                left_side_conduction_E = [self.bands[self.conduction_band_label_index][x] for x in range(k_index-sigma+1, k_index+1)]
+                left_side_conduction_E_ori = left_side_conduction_E.copy()
+                left_side_kpoint_dist = [self.k_dist[x] for x in range(k_index-sigma+1, k_index+1)]
+            left_side_kpoint_dist_delta = left_side_kpoint_dist[-1] - left_side_kpoint_dist[-2]
+            #appending
+            for x in range(len(left_side_conduction_E_ori)-2, -1, -1):
+                left_side_conduction_E.append(left_side_conduction_E_ori[x])
+                left_side_kpoint_dist.append(left_side_kpoint_dist[-1]+left_side_kpoint_dist_delta)
+
+            right_side_valance_E = [self.bands[self.valance_band_label_index][x] for x in range(k_index, k_index+sigma)]
+            right_side_valance_E_ori = right_side_valance_E.copy()
+            # prepending
+            for x in range(1,len(right_side_valance_E)):
+                right_side_valance_E.insert(0,right_side_valance_E_ori[x])
+            if high_symmetry_point:
+                left_side_valance_E = [self.bands[self.valance_band_label_index][x] for x in range(k_index-sigma, k_index)]
+                left_side_valance_E_ori = left_side_valance_E.copy()
+            else:
+                left_side_valance_E = [self.bands[self.valance_band_label_index][x] for x in range(k_index-sigma+1, k_index+1)]
+                left_side_valance_E_ori = left_side_valance_E.copy()
+            #appending
+            for x in range(len(left_side_valance_E_ori)-2, -1, -1):
+                left_side_valance_E.append(left_side_valance_E_ori[x])
+
+            fit_para_left_conduction = np.polyfit(left_side_kpoint_dist, left_side_conduction_E, 2)
+            fit_para_right_conduction = np.polyfit(right_side_kpoint_dist, right_side_conduction_E, 2)
+            fit_para_left_valance = np.polyfit(left_side_kpoint_dist, left_side_valance_E, 2)
+            fit_para_right_valance = np.polyfit(right_side_kpoint_dist, right_side_valance_E, 2)
+
+            electron_effective_mass_left = scipy.constants.hbar**2/(2*fit_para_left_conduction[0]*scipy.constants.m_e)  # The two on the denominator is from the 2 that comes from differentiating a 2 order polynomial
+            electron_effective_mass_right = scipy.constants.hbar**2/(2*fit_para_right_conduction[0]*scipy.constants.m_e)  # The two on the denominator is from the 2 that comes from differentiating a 2 order polynomial
+            hole_effective_mass_left = scipy.constants.hbar**2/(2*fit_para_left_valance[0]*scipy.constants.m_e)  # The two on the denominator is from the 2 that comes from differentiating a 2 order polynomial
+            hole_effective_mass_right = scipy.constants.hbar**2/(2*fit_para_right_valance[0]*scipy.constants.m_e)  # The two on the denominator is from the 2 that comes from differentiating a 2 order polynomial
+
+            return_dict = {}
+            return_dict.update({"fit_para_conduction_l":fit_para_left_conduction})
+            return_dict.update({"fit_para_conduction_r":fit_para_right_conduction})
+            return_dict.update({"fit_para_valance_l":fit_para_left_valance})
+            return_dict.update({"fit_para_valance_r":fit_para_left_valance})
+            return_dict.update({"me_l":electron_effective_mass_left})
+            return_dict.update({"me_r":electron_effective_mass_right})
+            return_dict.update({"mh_l":hole_effective_mass_left})
+            return_dict.update({"mh_r":hole_effective_mass_right})
+            return_dict.update({"bottom_of_conduction_l": fit_para_left_conduction[2]})
+            return_dict.update({"bottom_of_conduction_r":fit_para_right_conduction[2]})
+            return_dict.update({"top_of_valance_l":fit_para_left_valance[2]})
+            return_dict.update({"top_of_valance_r":fit_para_right_valance[2]})
+
+            p_era_LDA_ES = np.poly1d(fit_para_left_conduction)
+            fit_curve_left_conduction = p_era_LDA_ES(left_side_kpoint_dist)
+            p_right_conduction = np.poly1d(fit_para_right_conduction)
+            fit_curve_right_conduction = p_right_conduction(right_side_kpoint_dist)
+            
+            p_left_valance = np.poly1d(fit_para_left_valance)
+            fit_curve_left_valance = p_left_valance(left_side_kpoint_dist)
+            p_right_valance = np.poly1d(fit_para_right_valance)
+            fit_curve_right_valance = p_right_valance(right_side_kpoint_dist)
+            import matplotlib.pyplot as plt 
+            plt.plot(left_side_kpoint_dist, fit_curve_left_conduction, label=f"Left Conduction fit")
+            plt.plot(left_side_kpoint_dist, left_side_conduction_E, label=f"Left Conduction")
+            plt.plot(right_side_kpoint_dist, fit_curve_right_conduction, label=f"Right Conduction fit")
+            plt.plot(right_side_kpoint_dist, right_side_conduction_E, label=f"Right Conduction")
+            plt.plot(left_side_kpoint_dist, fit_curve_left_valance, label=f"Left Valance fit")
+            plt.plot(left_side_kpoint_dist, left_side_valance_E, label=f"Left Valance")
+            plt.plot(right_side_kpoint_dist, fit_curve_right_valance, label=f"Right Valance fit")
+            plt.plot(right_side_kpoint_dist, right_side_valance_E, label=f"Right Valance")
+            plt.legend()
+            if show_plot: plt.show()
+
+            return return_dict
+
+        if direction:
+            pass
