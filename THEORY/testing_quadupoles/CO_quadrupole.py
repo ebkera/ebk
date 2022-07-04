f227fced2cb6a0647d6ecb6a887c4c14387c5bd4
ebkera <ebk_era@hotmail.com>
2022-07-04 18:17:58 -0500

    added option of real grid of atoms

--- THEORY/testing_quadupoles/CO_quadrupole.py
+++ THEORY/testing_quadupoles/CO_quadrupole.py
@@ -20,10 +20,18 @@ class OCCO_quadrupole():
         self.point_charges = []  # will contain all the charges in this format: [[x,y,z], 1/-1] ([position],charge)
 
     def set_positions_for_zz(self, CC_distance:float) -> None:
+        self.point_charges = []  # will contain all the charges in this format: [[x,y,z], 1/-1] ([position],charge)
         self.point_charges.append([[0,0,-CC_distance/2],1*self.charge_on_a_pointcharge])
         self.point_charges.append([[0,0,CC_distance/2],1*self.charge_on_a_pointcharge])
         self.point_charges.append([[0,0,-CC_distance/2-self.CO_dist],-1*self.charge_on_a_pointcharge])
         self.point_charges.append([[0,0,CC_distance/2+self.CO_dist],-1*self.charge_on_a_pointcharge])
+        
+    def set_positions_actual_ionic(self, CC_distance:float) -> None:
+        self.point_charges = []  # will contain all the charges in this format: [[x,y,z], 1/-1] ([position],charge)
+        self.point_charges.append([[0,0,-CC_distance/2],4])
+        self.point_charges.append([[0,0,CC_distance/2],4])
+        self.point_charges.append([[0,0,-CC_distance/2-self.CO_dist],6])
+        self.point_charges.append([[0,0,CC_distance/2+self.CO_dist],6])
         
     def calculate_quadrupoles(self) -> list:
         self.Q = np.zeros((3,3))
@@ -33,6 +41,7 @@ class OCCO_quadrupole():
         self.Q_ionic = np.zeros((3,3))
         self.Q_ionic_non_traceless = np.zeros((3,3))
         for point_charge in self.point_charges:
+            # print(point_charge)
             r_vec = point_charge[0]
             r2 = np.dot(r_vec, r_vec)
             for col in range(0,3):
@@ -50,24 +59,36 @@ class OCCO_quadrupole():
                         self.Q_ionic[row,col]               += point_charge[1]*(3*(point_charge[0][row])*(point_charge[0][col]) - r2*f)
                         self.Q_ionic_non_traceless[row,col] += point_charge[1]*((point_charge[0][row])*(point_charge[0][col]))
 
+                    # if col == row:
+                        # print("traceless row:", row, point_charge[1], (3*(point_charge[0][row])*(point_charge[0][col]) - r2*f))
+                        # print("nontraceless :", row, point_charge[1], ((point_charge[0][row])*(point_charge[0][col])))
+        # print(self.Q)
+        # print(self.Q_non_traceless)
+
     def convert_to_debye(self):
         unit_factor_Debye = 1/0.2081943 #(same as c.elementary_charge*10**(-10)/(3.33564*10**(-30)))
-        self.Q_debye = self.Q*unit_factor_Debye
+        # print("printing the Q and debye")
+        # print(self.Q)
+        # print(self.Q_non_traceless)
+        # print(self.Q_debye)
+        self.Q = self.Q*unit_factor_Debye
         self.Q_non_traceless = self.Q_non_traceless*unit_factor_Debye
-        self.Q_electronic_debye = self.Q_electronic*unit_factor_Debye
+        self.Q_electronic = self.Q_electronic*unit_factor_Debye
         self.Q_electronic_non_traceless = self.Q_electronic_non_traceless*unit_factor_Debye
-        self.Q_ionic_debye = self.Q_ionic*unit_factor_Debye
+        self.Q_ionic = self.Q_ionic*unit_factor_Debye
         self.Q_ionic_non_traceless = self.Q_ionic_non_traceless*unit_factor_Debye
 
     def print_values_to_console(self):
         # Some testing prints
         print("\nCalculated values:\n")
-        print("dipoles from siesta (DA, eA): ", self.CO_dipole_from_siesta, self.CO_dipole_in_eA)
+        print("dipoles from siesta (DA, eA): ", self.CO_dipole_from_siesta,"\n", self.CO_dipole_in_eA)
         unit_factor_Debye = 1/0.2081943 #(same as c.elementary_charge*10**(-10)/(3.33564*10**(-30)))
-        print(f"Q in eA          : \n{self.Q}")
-        print(f"Qtraceless in eA : \n{self.Q_non_traceless}")
-        Q = self.Q*unit_factor_Debye
-        Q_non_traceless = self.Q_non_traceless*unit_factor_Debye
-        print(f"Q in D.A         : \n{Q}")
-        print(f"Qtraceless in D.A: \n{Q_non_traceless}")
+        print(f"Q in       : \n{self.Q}")
+        print(f"Q_non_traceless : \n{self.Q_non_traceless}")
+        # print(f"Q in eA          : \n{self.Q}")
+        # print(f"Qtraceless in eA : \n{self.Q_non_traceless}")
+        # Q = self.Q*unit_factor_Debye
+        # Q_non_traceless = self.Q_non_traceless*unit_factor_Debye
+        # print(f"Q in D.A         : \n{Q}")
+        # print(f"Qtraceless in D.A: \n{Q_non_traceless}")
 
