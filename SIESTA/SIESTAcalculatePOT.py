bb464249f1bff916116c740319c06af59e5b4636
ebkera <ebk_era@hotmail.com>
2022-11-07 19:00:28 -0600

    fixed bugs on coordinate positions

--- SIESTA/SIESTAcalculatePOT.py
+++ SIESTA/SIESTAcalculatePOT.py
@@ -11,12 +11,9 @@ class SIESTAcalculatePOT(SiestaReadOut):
         self.eps_r_2 = kwargs.get("eps_r_2",1)
         self.eps_1 = kwargs.get("eps_1",cons.epsilon_0)*self.eps_r_1
         self.eps_2 = kwargs.get("eps_2",cons.epsilon_0)*self.eps_r_2
-
-        # self.load_quadrupole_moments("/home/ebk/OneDrive_UIC/CQD Research/Analysis/Quadrupole_fitting/TPDAc_F_B3.electrostatics_edit.out")
         self.load_quadrupole_moments()
-        # print(self.get_pot_difference())
 
-    def calculate_pointcharge_coordinates(self):
+    def calculate_pointcharge_coordinates(self, plot=False):
         """This method will calculate the coordinates for the disccrete charge densities"""
         self.point_charge_coordinates = []
         self.point_charge_charge = [1,-1,-1,1]
@@ -26,8 +23,8 @@ class SIESTAcalculatePOT(SiestaReadOut):
         self.q_p = float(self.total_ionic_charge/2)
 
         def get_pos(Q,q):
-            return np.sqrt(Q/q)
-        
+            return np.sqrt(Q*0.2081943/q)
+
         positive_point_charge_coordinates = []
         negative_point_charge_coordinates = []
         for i in range(0,3):
@@ -46,7 +43,7 @@ class SIESTAcalculatePOT(SiestaReadOut):
             else:
                 Qxz = self.Q_ionic_non_traceless[0][2]
                 Qyz = self.Q_ionic_non_traceless[1][2]
-        
+
             # Figuring out the sign on the coordinates
             if (Qxz/self.point_charge_charge[i])<0 :
                 self.point_charge_coordinates[i][0]=self.point_charge_coordinates[i][0]*(-1)
@@ -57,36 +54,38 @@ class SIESTAcalculatePOT(SiestaReadOut):
         self.point_charge_coordinates.insert(0,[-self.point_charge_coordinates[-2][0],-self.point_charge_coordinates[-2][1],-self.point_charge_coordinates[-2][2]])
         self.point_charge_coordinates.insert(0,[-self.point_charge_coordinates[-1][0],-self.point_charge_coordinates[-1][1],-self.point_charge_coordinates[-1][2]])
 
-        fig,ax = plt.subplot_mosaic("AA;BC")
-        fig.suptitle(self.SystemLabel)
-        for i,x in enumerate(self.point_charge_coordinates):
-            if i in [0,3]:s="+"
-            else : s="."
-            ax['A'].plot(x[2],x[0],s,label=f"{i+1}")
-            ax['A'].set_xlabel("z")
-            ax['A'].set_ylabel("x")
+        if plot:
+            fig,ax = plt.subplot_mosaic("AA;BC")
+            fig.suptitle(self.SystemLabel)
+            for i,x in enumerate(self.point_charge_coordinates):
+                if i in [0,3]:s="+"
+                else : s="."
+                ax['A'].plot(x[2],x[0],s,label=f"{i+1}")
+                ax['A'].set_xlabel("z")
+                ax['A'].set_ylabel("x")
 
-            ax['B'].plot(x[0],x[1],s)
-            ax['B'].set_xlabel("x")
-            ax['B'].set_ylabel("y")
+                ax['B'].plot(x[0],x[1],s)
+                ax['B'].set_xlabel("x")
+                ax['B'].set_ylabel("y")
 
-            ax["C"].plot(x[2],x[1],s)
-            ax["C"].set_xlabel("z")
-            ax["C"].set_ylabel("y")
-        ax["A"].legend(loc="upper center")
-        fig.tight_layout()
-        fig.savefig(f"{self.SystemLabel}.pdf")
+                ax["C"].plot(x[2],x[1],s)
+                ax["C"].set_xlabel("z")
+                ax["C"].set_ylabel("y")
+            ax["A"].legend(loc="upper center")
+            fig.tight_layout()
+            fig.savefig(f"{self.SystemLabel}.pdf")
+            # fig.close()
 
     def calculate_coc_quadrupole(self):
         self.coc_positive_quadrupole = np.zeros((3,3))
         self.coc_negative_quadrupole = np.zeros((3,3))
-        for i,charge in enumerate(self.point_charge_coordinates):
+        for i,coordinates in enumerate(self.point_charge_coordinates):
             for row in range(0,3):
                 for col in range(0,3):
                     if i == 0 or i == 3:
-                        self.coc_positive_quadrupole[row,col] += self.q_p*((charge[row])*(charge[col]))
+                        self.coc_positive_quadrupole[row,col] += self.q_p*((coordinates[row])*(coordinates[col]))/0.2081943  # also converting back to D.A
                     else:
-                        self.coc_negative_quadrupole[row,col] += self.q_n*((charge[row])*(charge[col]))
+                        self.coc_negative_quadrupole[row,col] += self.q_n*((coordinates[row])*(coordinates[col]))/0.2081943
         self.total_coc_quadrupole = self.coc_negative_quadrupole+self.coc_positive_quadrupole
 
     def get_potential_at_point_from_plate():
@@ -95,7 +94,7 @@ class SIESTAcalculatePOT(SiestaReadOut):
     def get_pot_difference(self):
         a,b = [self.get_initial_cell_vectors()[0][0]*10**(-10),self.get_initial_cell_vectors()[1][1]*10**(-10)]
         c = self.get_initial_cell_vectors()[2][2]
-        f = 0.2081943*10**(-10)*10**(-10)  # factor fr converting the quadurupole units to 
+        f = 0.2081943*10**(-10)*10**(-10)  # factor for converting the quadurupole units to em^2 and not eA^2
         first_term = cons.elementary_charge*f*self.Q_non_traceless[2][2]*(1/self.eps_1 + 1/self.eps_2)/(2*a*b*(np.sqrt(f*self.Q_electronic_non_traceless[2][2]/self.total_normalized_electronic_charge) + np.sqrt(f*self.Q_ionic_non_traceless[2][2]/self.total_ionic_charge)))
 
         def getA(k1_max,k2_max):
@@ -106,7 +105,6 @@ class SIESTAcalculatePOT(SiestaReadOut):
                     # A+=np.e**
         getA(5,5)
         total = first_term
-        # print(a,b)
         return total
 
 
